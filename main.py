b819ba3848a3bf15ce95f069d30554695f7669bd
Alina <123@gmail.com>
2021-11-18 22:04:23 +0500

    Refactor code

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ class Interval:
     # когда ты пишешь self ты просишь класс переменную с каким-то именем
     # если ты указала self - то менять перемнную можно в любом методе
     def __init__(self, first_point, last_point):
+        if first_point > last_point:
+            self.first_point, self.last_point = None, None
+            return
         self.first_point = first_point
         self.last_point = last_point
-        print(f"[{first_point}, {last_point}]")
 
     def empty(self):
-        if self.first_point == None and self.last_point == None:
-            return True
-        return False
+        return self.first_point is None and self.last_point is None
 
     def cross(self, other):
         left_point = max(self.first_point, other.first_point)
@@ -20,9 +20,7 @@ class Interval:
         return Interval(left_point, right_point)
 
     def __eq__(self, other):
-        if self.first_point == other.first_point and self.last_point == other.last_point:
-            return True
-        return False
+        return self.first_point == other.first_point and self.last_point == other.last_point
 
     def point_in_interval(self, point):
         for i in range(self.first_point, self.last_point + 1):
