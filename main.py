f68f7e88814a302f800108f4febdff1ecb581082
Alina <123@gmail.com>
2021-11-18 21:39:32 +0500

    Implement cross function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Interval:
     def __init__(self, first_point, last_point):
         self.first_point = first_point
         self.last_point = last_point
+        print(f"[{first_point}, {last_point}]")
 
     def empty(self):
         if self.first_point == None and self.last_point == None:
@@ -12,35 +13,48 @@ class Interval:
         return False
 
     def cross(self, other):
+        left_point = max(self.first_point, other.first_point)
+        right_point = min(self.last_point, other.last_point)
+        if left_point > right_point:
+            left_point, right_point = None, None
+        return Interval(left_point, right_point)
 
-        print(other.first_point)
+    def __eq__(self, other):
+        if self.first_point == other.first_point and self.last_point == other.last_point:
+            return True
+        return False
 
-    # vcs
-    # share project on github
-    # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+    def point_in_interval(self, point):
+        for i in range(self.first_point, self.last_point + 1):
+            if point == i:
+                return True
+        return False
 
-    # Create git repository (optional)
-    # Ctrl + K  (и 1 и 2 фаза)
-    # Ctrl + shift + k
+# a = Interval(4, 10)
+# b = Interval(6, 11)
+# point = int(input())
+# a.cross(b)
+# a.point_in_interval(4, 10)
+# print(a == b)
+# vcs
+# share project on github
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 
-
-
-
-    # 3 фазы
-    # 1 (выбираешь файлы для архива) git add - выбираешь файлы которые хочешь запомнить
-    # 2 (создаёшь архив) git commit - фиксируешь изменения локально и пишешь комментарий почему ты изменила эти фалы (делаешь коммит - можно рассматривать как архив)
-    # 3 (отправляешь архив на гихаб) git push - отправляешь коммит в гитхаю
-
-    # 1
-    # добавить в stage area
-    # ctrl + K
-
-    # i = Interval(None, None)
-    # k = Interval(4, 8)
-    # print(i.empty())
-    # print(k.empty())
+# Create git repository (optional)
+# Ctrl + K  (и 1 и 2 фаза)
+# Ctrl + shift + k
 
 
-a = Interval(4, 10)
-b = Interval(6, 11)
-a.cross(b)
+# 3 фазы
+# 1 (выбираешь файлы для архива) git add - выбираешь файлы которые хочешь запомнить
+# 2 (создаёшь архив) git commit - фиксируешь изменения локально и пишешь комментарий почему ты изменила эти фалы (делаешь коммит - можно рассматривать как архив)
+# 3 (отправляешь архив на гихаб) git push - отправляешь коммит в гитхаю
+
+# 1
+# добавить в stage area
+# ctrl + K
+
+# i = Interval(None, None)
+# k = Interval(4, 8)
+# print(i.empty())
+# print(k.empty())
--- test.py
+++ test.py
@@ -12,3 +12,26 @@ def test_empty():
 def test_empty_2():
     i = Interval(4, 8)
     assert i.empty() == False
+
+
+def test_point_in_interval():
+    i = Interval(4, 12)
+    point = 5
+    assert i.point_in_interval(point) == True
+
+
+def test_eq():
+    a = Interval(4, 10)
+    b = Interval(4, 10)
+    assert a == b
+
+range_cross_data = [
+    (Interval(2, 8), Interval(3, 5), Interval(3, 5)),
+    (Interval(4, 7), Interval(1, 3), Interval(None, None)),
+    (Interval(4, 9), Interval(5, 11), Interval(5, 9)),
+]
+
+
+@pytest.mark.parametrize("interval1, interval2, expected_result", range_cross_data)
+def test_cross(interval1, interval2, expected_result):
+    assert interval1.cross(interval2) == expected_result
